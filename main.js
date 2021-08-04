54df35e381d8c60af7ba0cb54cfb6bb61dd0d2d5
Thomas <thomasyao15@gmail.com>
2021-08-04 23:49:37 +1000

    Added all winning combos, finished everything

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const gameBoard = (function () {
     let _gameBoardArray = ["", "", "", "", "", "", "", "", ""];
     const _winningCombinations = [
         [0, 1, 2],
-        [0, 3, 6]
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 4, 6],
+        [2, 5, 8],
+        [3, 4, 5],
+        [6, 7, 8],
     ];
 
     function _isWinningCombination(playerSign) {
